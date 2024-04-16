c2cb9b071c45f931cb464273fa2fbaad4ef92e8a
eduildo <51817346+Eduildo@users.noreply.github.com>
2024-04-16 17:05:51 +0100

    is_digit

---
+++ libft/ft_isdigit.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eduildo <eduildo@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 16:52:54 by eduildo           #+#    #+#             */
+/*   Updated: 2024/04/16 17:04:19 by eduildo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int ft_isdigit(int c)
+{
+
+    if (c >= '0' && c <= '9')
+        return (c);
+
+    return (0);
+}
+
+int main()
+{
+
+    char digit[10] = "Hello2";
+    int i;
+
+    for (int i = 0; digit[i] != '\0'; i++)
+    {
+        if (ft_isdigit(digit[i]) == 0)
+        {
+            printf("The return value is not a digit: %d\n", ft_isdigit(digit[i]));
+        }
+        else
+        {
+            printf("The return value is %d\n", ft_isdigit(digit[i]));
+        }
+    }
+}
