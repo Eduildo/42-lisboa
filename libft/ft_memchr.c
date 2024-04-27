3b0d62bec3ef9ed0d18530769739c3e78b27b519
eduildo <51817346+Eduildo@users.noreply.github.com>
2024-04-27 16:32:35 +0100

    memchr && strncmp

---
+++ libft/ft_memchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eduildo <eduildo@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/27 16:16:22 by eduildo           #+#    #+#             */
+/*   Updated: 2024/04/27 16:24:49 by eduildo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+void *ft_memchr(const void *s, int c, size_t len)
+{
+    unsigned char *str;
+    size_t len i;
+    unsigned char uc;
+
+    str = (unsigned char *)s;
+    uc(unsigned char);
+    i = 0;
+    while (i < len)
+    {
+        if (str[i] == uc)
+            return ((void *)&str[i]);
+        i++;
+    }
+    return (NULL);
+}
---
+++ libft/ft_strncmp.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eduildo <eduildo@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/27 16:02:38 by eduildo           #+#    #+#             */
+/*   Updated: 2024/04/27 16:11:16 by eduildo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int ft_strncmp(const char *s1, const char *s2, size_t len)
+{
+    size_t i;
+    i = 0;
+    while ((s1[i] || s2[i]) && i < len)
+    {
+        if ((unsigned char)s1[i] != (unsigned char)s2[i])
+            return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+        i++;
+    }
+    return (0);
+}
